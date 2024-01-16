41ba39da3ba372dbbef9d1ef9721d78f9c464169
Alex Taylor <ataylor@posteo.us>
2024-01-15 20:17:02 -0500

    first commit (copy wireguard config)

---
+++ .get-become-pass.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -eou pipefail
+bw get password 5bd6590a-849e-40b1-87bf-b0f900122a01
---
+++ .get-vault-pass.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -eou pipefail
+bw get password df0aa388-f5c8-4307-baa7-b0f900149ab5
